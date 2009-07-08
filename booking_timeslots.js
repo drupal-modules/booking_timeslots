944f771871a84184363117e99d25d51358c0176d
Rafal <kenorb@191974.no-reply.drupal.org>
2009-07-08 13:04:24 +0000

    #505052 by kenorb: fixed wrong format of events

--- booking_timeslots.js
+++ booking_timeslots.js
@@ -2,25 +2,8 @@ if (Drupal.jsEnabled) {
   // When the DOM is ready, try an AJAX content load
   
   $(document).ready(function() {
-    // Start date
-    input_obj = $('.container-inline-date').find("input");
-
-    if (!(input_obj.eq(0).datepicker === undefined))
-    input_obj.eq(0).datepicker({ 
-        showStatus: true,
-        onSelect: function(date) { 
-            input_obj.eq(2).val(date)
-        } 
-    });
-
-    // Date To
-    if (!(input_obj.eq(2).datepicker === undefined))
-    input_obj.eq(2).datepicker({ 
-        showStatus: true,
-        onSelect: function(date) { 
-            input_obj.eq(0).val(date)
-        } 
-    });
-})
+    $('.container-inline-date').find("input").eq(0).change(function(){$('.container-inline-date').find("input").eq(2).val(this.value)});
+    $('.container-inline-date').find("input").eq(2).change(function(){$('.container-inline-date').find("input").eq(0).val(this.value)});
+  })
 };
 
