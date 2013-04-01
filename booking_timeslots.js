d6ef52821796bd7112eba6ce45208e6cd5d417bf
Rafal <kenorb@gmail.com>
2013-04-01 13:57:39 +0100

    Converting 6.x to 7.x based on the core requirements. Still needs some work

--- booking_timeslots.js
+++ booking_timeslots.js
@@ -1,9 +1,8 @@
-if (Drupal.jsEnabled) {
-  // When the DOM is ready, try an AJAX content load
+(function ($) {
   
   $(document).ready(function() {
     $('.container-inline-date').find("input").eq(0).change(function(){$('.container-inline-date').find("input").eq(2).val(this.value)});
     $('.container-inline-date').find("input").eq(2).change(function(){$('.container-inline-date').find("input").eq(0).val(this.value)});
   })
-};
-
+  
+})(jQuery);
